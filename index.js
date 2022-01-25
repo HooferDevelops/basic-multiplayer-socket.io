ef0dfbe777cf6d7fa9361ddfe5d8629ca3dee1dd
boogameow <76601381+boogameow@users.noreply.github.com>
2022-01-24 18:50:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function template(id){
     if (!id) id = "";
     return {
         id: id,
-        y: 0
+        y: 0,
+        dir: 0,
     }
 }
 
@@ -31,7 +32,7 @@ io.on('connection', (socket) => {
     users[socket.id] = user;
     /* on player leaving */
     socket.on("disconnect", ()=>{
-        io.emit("updatePosition", {id: socket.id, y:360});
+        io.emit("updatePosition", {id: socket.id, y:360, dir: 0});
         // hacky but it works, i mean it's a snap game ^
         delete users[socket.id];
         console.log("User Disconnected");
@@ -39,12 +40,7 @@ io.on('connection', (socket) => {
     /* movement packet */
     socket.on("updatePosition", (data)=> {
         users[socket.id].y = data.contents[0]
-        io.emit("updatePosition", users[socket.id])
-    })
-    /* depricated */
-    socket.on("updateRot", (data)=>{
-        console.log("ROT")
-        users[socket.id].rot = data
+        users[socket.id].dir = data.contents[1]
         io.emit("updatePosition", users[socket.id])
     })
     io.emit('userJoin', users[socket.id])
