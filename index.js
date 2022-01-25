95d9aa0ce037a1dcc3289d9be9306859ccea0672
boogameow <76601381+boogameow@users.noreply.github.com>
2022-01-24 18:33:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,13 @@ function template(id){
     if (!id) id = "";
     return {
         id: id,
-        color: "#000000",
-        x: 0, y: 0, mx: 0, my: 0
+        y: 0
     }
 }
 
 /* ONLINE USER LIST */
 
-var users = {
-
-}
+var users = {}
 
 /* MOVEMENT + PLAYER SYSTEM */
 /* INFO:
@@ -30,22 +27,18 @@ var users = {
 io.on('connection', (socket) => {
     console.log("User Connected");
     var user = new template(socket.id);
-    user.color = "#" + Math.floor(Math.random()*16777215).toString(16);
     user.id = socket.id;
     users[socket.id] = user;
     /* on player leaving */
     socket.on("disconnect", ()=>{
-        io.emit("updatePosition", {id: socket.id, color:"#000000", x:0,y:270, mx: 0, my: 0});
+        io.emit("updatePosition", {id: socket.id, y:360});
         // hacky but it works, i mean it's a snap game ^
         delete users[socket.id];
         console.log("User Disconnected");
     })
     /* movement packet */
     socket.on("updatePosition", (data)=> {
-        users[socket.id].x = data.contents[0]
-        users[socket.id].y = data.contents[1]
-        users[socket.id].mx = data.contents[2]
-        users[socket.id].my = data.contents[3]
+        users[socket.id].y = data.contents[0]
         io.emit("updatePosition", users[socket.id])
     })
     /* depricated */
@@ -62,63 +55,3 @@ io.on('connection', (socket) => {
         }
     });
 })
-
-/* BASIC AI BOT */
-/* INFO:
-   basically this bot moves up and down
-   it's look direction is 100% random
-*/
-
-var bot = new template("BOT");
-users["BOT"] = bot;
-users["BOT"].y = 25;
-
-
-setInterval(function(){
-    users["BOT"].color = "#" + Math.floor(Math.random()*16777215).toString(16);
-    io.emit("updatePosition", users["BOT"])
-}, 100)
-
-
-
-function down(){
-    setTimeout(function(){
-        users["BOT"].y--;
-        io.emit("updatePosition", users["BOT"])
-        if (users["BOT"].y > 0){
-            down();
-        } else {
-            up();
-        }
-        users["BOT"].mx = Math.floor(Math.random() * 270);
-        users["BOT"].my = Math.floor(Math.random() * 270);
-        if (Math.random() > 0.5) {
-            users["BOT"].my*=-1;
-        } else {
-            users["BOT"].mx*=-1;
-        }
-    },100)
-}
-
-function up(){
-    setTimeout(function(){
-        users["BOT"].y++;
-        io.emit("updatePosition", users["BOT"])
-        if (users["BOT"].y < 25){
-            up();
-        } else {
-            down();
-        }
-        users["BOT"].mx = Math.floor(Math.random() * 270);
-        users["BOT"].my = Math.floor(Math.random() * 270);
-        if (Math.random() > 0.5) {
-            users["BOT"].mx*=-1;
-        } else {
-            users["BOT"].my*=-1;
-        }
-        
-    },100)
-}
-
-down();
-
